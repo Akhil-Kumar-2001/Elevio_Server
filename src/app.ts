cfed93e82b56577f6aabb75337c35b7204157c1f
Akhil-Kumar-2001 <akhilkumars2001@gmail.com>
2025-05-13 17:31:54 +0530

    fix:updated app.ts file

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,8 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morganMiddleware);
